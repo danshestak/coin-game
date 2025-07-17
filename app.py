271de7d20c4aeceaf77ce6e1f3cb85f3824c058c
Dan Shestak <187454689+danshestak@users.noreply.github.com>
2025-07-17 18:04:18 -0400

    added timestamp to game datamodel class and created database

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import uuid
+import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(36), nullable=False)
     finished = db.Column(db.Boolean, default=False, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.now(datetime.timezone.utc))
     rounds = db.relationship("Round", backref="game", lazy=True)
 
 class Round(db.Model):
@@ -19,6 +21,9 @@ class Round(db.Model):
     value = db.Column(db.Integer, nullable=False)
     deltaTime = db.Column(db.Float, nullable=False)
     game = db.Column(db.Integer, db.ForeignKey("game.id"), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["POST", "GET"])
 def index():
